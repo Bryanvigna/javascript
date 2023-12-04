5c0bbe4e2a08e749b2c88b8a7c356e924aab2813
Bryan vigna <bryanvigna.p@gmail.com>
2023-12-04 15:40:30 -0300

    um pouco sobre condicionais em JS

---
+++ ModuloC/ex08.js
@@ -0,0 +1,6 @@
+var vel=60.5
+console.log(`A velocidade do carro é ${vel}Km/h`)
+if(vel > 60) {//condição simples
+    console.log(`Você ultrapassou a velocidade, SERÁS TAXADO!`)
+}
+console.log(`Dirija usando sempre cinto de segurança!`)
---
+++ ModuloC/ex09.js
@@ -0,0 +1,7 @@
+var país = 'EUA'
+console.log(`Vivendo em ${país}`)
+if(país == 'Brasil'){
+    console.log('oi Brasileiro')
+} else{
+    console.log('you is Gringo!')
+}
