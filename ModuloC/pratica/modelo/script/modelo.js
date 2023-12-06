5d5e6fdf0397805c3d36591bfb4a0bc22425bcca
Bryan vigna <bryanvigna.p@gmail.com>
2023-12-06 15:32:40 -0300

    organização de arquivo

--- ModuloC/pratica/modelo/script/modelo.js
+++ ModuloC/pratica/modelo/script/modelo.js
@@ -1,20 +1,24 @@
 function carregar(){
     var msg=document.querySelector('div#msg')
-    var img=document.getElementById('img')
+    var img=document.querySelector('img#img')
     var data =new Date()
-    //var hora = data.getHours()
-    var hora=11
-    msg.innerHTML = `Agora são ${hora} horas`
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas<br>`
 
     if(hora >= 0 && hora<12){
         //dia!
-        img.src= 'manha.png'
+        msg.innerHTML += 'bom dia!'
+        img.src= 'imagens/manha.png'
         document.body.style.background = '#C2D3E6'
     }else if(hora >=12 && hora <18){
         //tarde!
-        img.src= 'tarde.png'
+        msg.innerHTML += 'boa tarde!'
+        img.src= 'imagens/tarde.png'
+        document.body.style.background = '#F0BB6C'
     }else{
         //noite!
-        img.src= 'noite.png'  
+        msg.innerHTML += 'boa noite!'
+        img.src= 'imagens/noite.png' 
+        document.body.style.background = '#B7A9C3' 
     }
 }
