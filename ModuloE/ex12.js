8ffc1a5cf4fdd9a4f7f068a9cd9aa75b326e7b45
Bryan vigna <bryanvigna.p@gmail.com>
2023-12-07 14:05:26 -0300

    repetição +depurador

---
+++ ModuloE/ex12.js
@@ -0,0 +1,21 @@
+var c=1
+
+do{
+    console.log(`passo ${c}`)
+    c++
+}while(c<=4)
+
+/*while(c<=3)*{
+    console.log(`passo ${c}`)
+    c++
+}/
+
+
+/*console.log('tudo certo?')
+console.log('tudo certo?')
+console.log('tudo certo?')
+console.log('tudo certo?')
+console.log('tudo certo?')
+console.log('tudo certo?')
+console.log('tudo certo?')
+*/
---
+++ ModuloE/ex13.js
@@ -0,0 +1,6 @@
+console.log('vai startar')
+
+for(var c=1; c<=5;c++){
+    console.log(`testando ${c}`)
+}
+console.log('final')
